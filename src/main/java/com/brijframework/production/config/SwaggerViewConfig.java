77f2f066cbd19bce72162e5e73c05effc7715d37
Ram Kishor <ram@omniesolutions.com>
2024-02-01 20:59:07 +0530

    ok

--- src/main/java/com/brijframework/production/config/SwaggerViewConfig.java
+++ src/main/java/com/brijframework/production/config/SwaggerViewConfig.java
@@ -1,9 +1,26 @@
 package com.brijframework.production.config;
 
-//@Configuration
-//@EnableSwagger2
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerViewConfig {
-/*
+
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
@@ -17,13 +34,13 @@ public class SwaggerViewConfig {
 		return new ApiKey("APIKey", "Authorization", "header");
 	}
 
-	private SecurityContext securityContext() {
-		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/.*")).build();
+	private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+		return springfox.documentation.spi.service.contexts.SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/.*")).build();
 	}
 
 	private List<SecurityReference> defaultAuth() {
 		final AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
 		final AuthorizationScope[] authorizationScopes = new AuthorizationScope[] { authorizationScope };
 		return Collections.singletonList(new SecurityReference("APIKey", authorizationScopes));
-	}*/
+	}
 }
